573d8ffb1ab66ebd38a6c25e1eb4865b18eaf145
Rani <ranichavre3@gmail.com>
2026-03-28 08:48:45 +0530

    Fixed API URL for production

--- frontend/leave-frontend/src/app/services/auth.service.ts
+++ frontend/leave-frontend/src/app/services/auth.service.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'https://leave-backend-bp79.onrender.com/api/users'; // Backend URL
+private apiUrl = 'https://leave-management-api.onrender.com/api/users';  // Backend URL
   private tokenKey = 'token';
 
   constructor(private http: HttpClient, private router: Router) {}
